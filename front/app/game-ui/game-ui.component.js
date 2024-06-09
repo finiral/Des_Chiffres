f8577d92ae6c9ede6d56372a16237cc36b1d82d5
finiral <finiral@yahoo.fr>
2024-06-09 20:24:53 +0300

    affichage des nb

--- front/app/game-ui/game-ui.component.js
+++ front/app/game-ui/game-ui.component.js
@@ -2,6 +2,8 @@ angular.module("gameUi",[])
     .component("gameUi",{
         templateUrl : "game-ui/game-ui.component.template.html",
         controller :["$scope","$interval", function ($scope,$interval){
+            $scope.singleDigit=13424
+            $scope.cells=[12,24,321,53,65]
             var self=this
             function initialize(){
                 self.seconds=60
